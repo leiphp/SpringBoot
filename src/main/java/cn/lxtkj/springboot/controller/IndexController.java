559b525c5d8e61b3a0e9571fe6a4e032e9dd26be
leiwen <leiwen@szqgly.com>
2019-08-14 20:04:10 +0800

    feat[配置]:新增多环境配置文件

--- src/main/java/cn/lxtkj/springboot/controller/IndexController.java
+++ src/main/java/cn/lxtkj/springboot/controller/IndexController.java
@@ -1,14 +1,29 @@
 package cn.lxtkj.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController//注解表示该类所以方法，都会返回json格式数据（@Controller+@ResponseBody）
 public class IndexController {
+    @Value("${name}")
+    private String name;
+    @Value("${http_url}")
+    private String httpUrl;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say(){
         return "hello world!";
+    }
+
+    @RequestMapping("/getName")
+    public String getName(){
+        return name;
+    }
+
+    @RequestMapping("/httpUrl")
+    public String httpUrl(){
+        return httpUrl;
     }
 }
