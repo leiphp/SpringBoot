b5f178f89bbd607d509536b2a61d94e4bebce2d2
leiwen <657106593@qq.com>
2019-08-16 23:27:11 +0800

    feat[分页]:整合pagehelper分页

--- src/main/java/cn/lxtkj/springboot/service/UserService.java
+++ src/main/java/cn/lxtkj/springboot/service/UserService.java
@@ -2,6 +2,8 @@ package cn.lxtkj.springboot.service;
 
 import cn.lxtkj.springboot.entity.User;
 import cn.lxtkj.springboot.mapper.UserMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +19,7 @@ public class UserService {
         return insertUserResult;
     }
 
-    public List<User> findUserList(int page, int pageSize){
+    public PageInfo<User> findUserList(int page, int pageSize){
         //mysql查询limit
         //pageHelper 帮我们生产分页语句
         PageHelper.startPage(page, pageSize);
