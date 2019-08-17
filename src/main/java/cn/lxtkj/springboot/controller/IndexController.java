b2db98074e6e89eb1d2077dd9695705de7589277
leiwen <leiwen@szqgly.com>
2019-08-17 18:17:25 +0800

    feat[分页]:mybatis分页

--- src/main/java/cn/lxtkj/springboot/controller/IndexController.java
+++ src/main/java/cn/lxtkj/springboot/controller/IndexController.java
@@ -1,5 +1,9 @@
 package cn.lxtkj.springboot.controller;
 
+import cn.lxtkj.springboot.entity.User;
+import cn.lxtkj.springboot.service.UserService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +15,9 @@ public class IndexController {
     private String name;
     @Value("${http_url}")
     private String httpUrl;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say(){
@@ -25,5 +32,9 @@ public class IndexController {
     @RequestMapping("/httpUrl")
     public String httpUrl(){
         return httpUrl;
+    }
+    @RequestMapping("/index")
+    public PageInfo<User> index(int page, int pageSize){
+        return userService.findUserList(page, pageSize);
     }
 }
